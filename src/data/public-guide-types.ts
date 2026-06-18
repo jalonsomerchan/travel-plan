ebf1cc212c6c1eb34bbcd10928c31b4e744d72db
Jorge Alonso <jalonsomerchan@gmail.com>
2026-06-18 07:09:22 +0200

    Add guide topic types

---
+++ src/data/public-guide-types.ts
@@ -0,0 +1,11 @@
+export interface PublicGuideTopic {
+  id: string;
+  slug: string;
+  label: string;
+  eyebrow: string;
+  title: string;
+  description: string;
+  problem: string;
+  useCase: string;
+  tips: string[];
+}
